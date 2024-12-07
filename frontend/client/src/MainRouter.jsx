fd77e81cd96d51f5796a7d83cd675695aed053e9
sduffney <148494340+sduffney@users.noreply.github.com>
2024-12-07 17:22:55 -0500

    move

--- frontend/client/src/MainRouter.jsx
+++ frontend/client/src/MainRouter.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
-import Home from './core/Home' 
-import Users from './user/Users.jsx'
-import Signup from './user/Signup.jsx'
-import Signin from './lib/Signin.jsx'
-import Profile from './user/Profile.jsx'
-import PrivateRoute from './lib/PrivateRoute.jsx'
-import EditProfile from './user/EditProfile.jsx'
-import Products from './product/Products.jsx'
-import ProductDetail from './product/ProductDetail.jsx'
+import Home from '../core/Home.jsx' 
+import Users from '../user/Users.jsx'
+import Signup from '../user/Signup.jsx'
+import Signin from '../lib/Signin.jsx'
+import Profile from '../user/Profile.jsx'
+import PrivateRoute from '../lib/PrivateRoute.jsx'
+import EditProfile from '../user/EditProfile.jsx'
+import Products from '../product/Products.jsx'
+import ProductDetail from '../product/ProductDetail.jsx'
 
-import Menu from './core/Menu' 
+import Menu from '../core/Menu.jsx' 
 function MainRouter() {
 return (
 <div>
